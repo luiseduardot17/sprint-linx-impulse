981f5cad328beca1d8f4e5cee3f541262516dca8
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-08-22 16:51:46 -0300

    criei o component Footer.jsx e seu css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Estoque from './components/Estoque/Estoque'
+import Footer from './components/Footer/Footer'
 import Form from './components/Form/Form'
 import Header from './components/Header/Header'
 import SectionEnd from './components/SectionEnd/SectionEnd'
@@ -12,6 +13,7 @@ function App() {
       <Form/>
       <Estoque/>
       <SectionEnd/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import S from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={S.footer}>
+      <h6>Testando suas habilidades em HTML, CSS e JS.</h6>
+      <h6>Linx Impulse</h6>
+      <h6>2022</h6>
+    </footer>
+  );
+};
+
+export default Footer;
