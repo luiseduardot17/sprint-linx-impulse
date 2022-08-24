6f2b3b3e99560ffdfa1f8d2af07ac7684aa2ae6d
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-08-23 23:45:12 -0300

    fix: resolução de um bug que não permitia expandir a grade de produtos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer className={S.footer}>
       <h6>Testando as habilidades em React</h6>
+      <h6>Luis Eduardo</h6>
       <a href="https://github.com/luiseduardot17" target="_blank" rel="noopener noreferrer">Meu github</a>
       <h6>Linx Impulse</h6>
       <h6>2022</h6>
